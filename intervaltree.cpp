6bd38b39c336820c50475ad6d64ae42926b46a35
bandlarakesh7 <bandlarakesh7@gmail.com>
2017-02-16 23:05:48 +0530

    Update intervaltree.cpp

--- intervaltree.cpp
+++ intervaltree.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
-
+ 
 struct Interval
 {
     int low, high;
@@ -8,22 +8,44 @@ struct Interval
  
 struct ITNode
 {
-    Interval *i;  // 'i' could also be a normal variable
+    Interval *i;  
     int max;
     ITNode *left, *right;
 };
  
-// A utility function to create a new Interval Search Tree Node
 ITNode * newNode(Interval i)
 {
     ITNode *temp = new ITNode;
     temp->i = new Interval(i);
     temp->max = i.high;
     temp->left = temp->right = NULL;
-    return temp;
-};
+return temp;};
  
-//A utility function to find the min value node 
+ITNode *insert(ITNode *root, Interval i)
+{
+    if (root == NULL)
+        return newNode(i);
+ 
+    int l = root->i->low;
+ 
+    if (i.low < l)
+        root->left = insert(root->left, i);
+ 
+    else
+        root->right = insert(root->right, i);
+ 
+    if (root->max < i.high)
+        root->max = i.high;
+ 
+    return root;
+}
+ 
+bool doOVerlap(Interval i1, Interval i2)
+{
+    if (i1.low <= i2.high && i2.low <= i1.high)
+        return true;
+    return false;
+}
 ITNode *minValueNode(ITNode *root)
 {
     ITNode *temp=root;
@@ -31,66 +53,19 @@ ITNode *minValueNode(ITNode *root)
     while(temp->left!=NULL)
         temp=temp->left;
     return temp;
-} 
-// A utility function to insert a new Interval Search Tree Node
-// This is similar to BST Insert.  Here the low value of interval
-// is used tomaintain BST property
-ITNode *insert(ITNode *root, Interval i)
+}  
+Interval *overlapSearch(ITNode *root, Interval i)
 {
-    // Base case: Tree is empty, new node becomes root
-    if (root == NULL)
-        return newNode(i);
+    if (root == NULL) return NULL;
  
-    // Get low value of interval at root
-    int l = root->i->low;
- 
-    // If root's low value is smaller, then new interval goes to
-    // left subtree
-    if (i.low < l)
-        root->left = insert(root->left, i);
- 
-    // Else, new node goes to right subtree.
-    else
-        root->right = insert(root->right, i);
- 
-    // Update the max value of this ancestor if needed
-    if (root->max < i.high)
-        root->max = i.high;
- 
-    return root;
-}
- 
-// A utility function to check if given two intervals overlap
-bool doOVerlap(Interval i1, Interval i2)
-{
-    if (i1.low <= i2.high && i2.low <= i1.high)
-        return true;
-    return false;
-}
- 
-// The main function that searches a given interval i in a given
-// Interval Tree.
-void overlapSearch(ITNode *root, Interval i)
-{
-    // Base Case, tree is empty
-    if (root == NULL) return ;
- 
-    // If given interval overlaps with root
     if (doOVerlap(*(root->i), i))
-    {
-        cout << "Overlaps with [" << root->i->low << ", " << root->i->high << "]\n";
-    }
- 
-    // If left child of root is present and max of left child is
-    // greater than or equal to given interval, then i may
-    // overlap with an interval is left subtree
+        return root->i;
+
     if (root->left != NULL && root->left->max >= i.low)
         return overlapSearch(root->left, i);
  
-    // Else interval can only overlap with right subtree
     return overlapSearch(root->right, i);
 }
-
 int maximum(int a,int b,int c)
 {
     if(a>=b&&a>=c)
@@ -131,13 +106,10 @@ ITNode *deleteInterval(ITNode *root,Interval x)
 {
     if(root==NULL)
         return root;
-    //if required interval is in left subtree
     if(x.low<root->i->low)
         root->left=deleteInterval(root->left,x);
-    //if required interval is in right subtree
-    else if(x.low<root->i->low)
+    else if(x.low>root->i->low)
         root->right=deleteInterval(root->right,x);
-    //if key is same as root's key
     else
     {
         if(root->left==NULL)
@@ -160,7 +132,7 @@ ITNode *deleteInterval(ITNode *root,Interval x)
     }
     return root;
 } 
- 
+
 void inorder(ITNode *root)
 {
     if (root == NULL) return;
@@ -171,12 +143,10 @@ void inorder(ITNode *root)
          << " max = " << root->max << endl;
  
     inorder(root->right);
-}
+}    
  
-// Driver program to test above functions
 int main()
 {
-    // Let us create interval tree shown in above figure
     Interval ints[] = {{15, 20}, {10, 30}, {17, 19},
         {5, 20}, {12, 15}, {30, 40}
     };
@@ -184,17 +154,22 @@ int main()
     ITNode *root = NULL;
     for (int i = 0; i < n; i++)
         root = insert(root, ints[i]);
+        
+        Interval x[] = {{6, 7},{14,16},{21,23}};
  
-    cout << "Inorder traversal of constructed Interval Tree is\n";
+ for(int i=0;i<3;i++)
+ {
+    cout << "[" << x[i].low << "," << x[i].high << "]";
+    Interval *res = overlapSearch(root, x[i]);
+    if (res == NULL)
+        cout << "overlaps with none\n";
+    else
+        cout << "overlaps with [" << res->low << ", " << res->high << "]\n";
+}   insert(root,{14,16});insert(root,{21,23});
+cout << endl<< "Inorder traversal of constructed Interval Tree is\n";
     inorder(root);
- 
-    Interval x = {14,16};
- 
-    cout << "\nSearching for interval [" << x.low << "," << x.high << "]\n";
-    overlapSearch(root, x);
-    Interval y= {10,30};
-    root=deleteInterval(root,y);
-    updateMax(root);
+    root=deleteInterval(root,{14,16});
+cout << endl<< "Inorder traversal of constructed Interval Tree is\n";
     inorder(root);
-    return 0;
+return 0;
 }
